e36398345abb9a131fa7fcc7e3615f83242d670e
qblassk <g.bulaschevich@gmail.com>
2023-03-01 20:01:44 -0300

    configuration

---
+++ src/database/config/config.js
@@ -0,0 +1,29 @@
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_USER = process.env.DB_USER || 'root';
+const DB_PASSWORD = process.env.DB_PASSWORD || null;
+const DB_NAME = process.env.DB_NAME || 'limadb';
+const DB_PORT = process.env.DB_PORT || '3306';
+
+module.exports = {
+   development: {
+      username: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      host: DB_HOST,
+      dialect: 'mysql',
+   },
+   test: {
+      username: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      host: DB_HOST,
+      dialect: 'mysql',
+   },
+   production: {
+      username: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      host: DB_HOST,
+      dialect: 'mysql',
+   },
+};
